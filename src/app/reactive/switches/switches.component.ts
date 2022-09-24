06eab3fb2dddafc4e86494adfd17125d93e4a1a4
ivani2 <eltalivanitodos@gmail.com>
2022-09-24 14:59:52 -0500

    fin de seccion 17

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -27,6 +27,27 @@ export class SwitchesComponent implements OnInit {
       ...this.persona,
       condiciones: false
     } );
+
+    // this.miFormulario.get( "condiciones" )?.valueChanges.subscribe( newValue => {
+    //   console.log( newValue );
+    // } );
+
+    this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest}) => {
+      // console.log( form );
+      // delete form.condiciones;
+      this.persona = rest;
+    } );
+
+
+
+  }
+
+
+  guardar(): void {
+    const formValue =  { ...this.miFormulario.value };
+    delete formValue.condiciones;
+    this.persona =  formValue;
+    console.log( formValue );
   }
 
 }
